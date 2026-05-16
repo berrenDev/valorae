3f59640d3e1384702a18f4af164ef1520e0b332b
AlexBerren <alexberren@gmail.com>
2026-05-17 01:13:56 +0200

    TO-DO

--- backend/src/main/java/com/gestionapp/backend/controller/CategoryController.java
+++ backend/src/main/java/com/gestionapp/backend/controller/CategoryController.java
@@ -20,6 +20,7 @@ public class CategoryController implements CategoriesApi {
     private final CategoryService categoryService;
     private final CategoryMapper categoryMapper;
 
+    //AÑADIR ENDPOINT LISTADO DE CATEGORIAS POR USUARIO
     @Override
     public ResponseEntity<CategoryResponse> createCategory(@RequestBody CreateCategoryRequest createCategoryRequest) {
         CategoryResponse createdCategory = categoryMapper.toApiDomain(categoryService.createCategory(categoryMapper.fromApiDomain(createCategoryRequest)));
